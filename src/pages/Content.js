ce5ea6ae7ffa6d86f669d0c646628fe2e8eb19f9
장영주 <corydalisss@gmail.com>
2024-02-20 00:01:34 +0900

    update text in main
    landscape -> portrait (ko && en)

--- src/pages/Content.js
+++ src/pages/Content.js
@@ -39,7 +39,7 @@ const Content = () => {
 
         <div className="for-pc">
           <h1>스마트폰 가로모드에서만 확인할 수 있습니다.</h1>
-          <h1>Sorry. Only available in mobile phone landscape view. </h1>
+          <h1>Sorry. Only available portrait view in mobile. </h1>
         </div>
 
         <div className="wrap">
