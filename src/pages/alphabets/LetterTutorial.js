cb863daa8dc8b6c4ec5e15767351bd290f6fe613
장영주 <corydalisss@gmail.com>
2024-03-02 17:30:22 +0900

    update tutorial ui (guide number btn)

--- src/pages/alphabets/LetterTutorial.js
+++ src/pages/alphabets/LetterTutorial.js
@@ -2,7 +2,14 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { QuestionBox } from "../../components/QuestionBox";
 import { useNavigate, useLocation, useMatch } from "react-router-dom";
-import letters from "../../data/letters.json";
+
+const sampleword = [
+  {
+    id: 0,
+    lettername: "A",
+    pronounce: ["아", "애", "어"],
+  },
+];
 const LetterTutorial = () => {
   const [obj, setObj] = useState("");
   const [list, setList] = useState([]);
@@ -96,21 +103,22 @@ const LetterTutorial = () => {
           <>
             {!newChk2 ? (
               <>
-                <span className="number">{highlightedIndex + 1}</span> 학습 단어
-                읽는 연습을 합니다. 아래에 단어를 추가하여 메모할 수 있습니다.
-                보이지 않을 경우 화면을 아래로 내려보세요.
+                <span className="number">{highlightedIndex + 1}</span>
+                <span>
+                  학습 단어 읽는 연습을 합니다. 아래에 단어를 추가하여 메모할 수
+                  있습니다. 보이지 않을 경우 화면을 아래로 내려보세요.
+                </span>
               </>
             ) : (
               <>
-                <span className="number">
-                  {highlightedIndex + 3} 이렇게 단어장에 추가됩니다.
-                  <button
-                    className={highlightedIndex ? "highlighted" : "hide"}
-                    onClick={ischecked}
-                  >
-                    화면 안내 종료
-                  </button>
-                </span>
+                <span className="number">{highlightedIndex + 3}</span>
+                <span>이렇게 단어장에 추가됩니다.</span>
+                <button
+                  className={highlightedIndex ? "highlighted" : "hide"}
+                  onClick={ischecked}
+                >
+                  화면 안내 종료
+                </button>
               </>
             )}
           </>
@@ -120,7 +128,8 @@ const LetterTutorial = () => {
       case 3:
         return (
           <>
-            <span className="number">{highlightedIndex + 1}</span>함 입력헤보셈
+            <span className="number">{highlightedIndex + 1}</span>하얀색 네모에
+            입력해보세요.
           </>
         );
         break;
@@ -158,92 +167,81 @@ const LetterTutorial = () => {
               </h3>
             </div>
 
-            {letters.letter
-              .filter((sampleitm) => sampleitm.lettername === "test")
-              .map((item) => {
-                return (
-                  <>
-                    {" "}
-                    <h1
-                      className={
-                        "letter timid" +
-                        (highlightedIndex === 0 ? " highlighted" : "")
-                      }
-                    >
-                      {item.lettername}
-                    </h1>
-                    <div
-                      className={
-                        "pronunciation-con timid" +
-                        (highlightedIndex === 1 ? " highlighted" : "")
-                      }
-                    >
-                      <div className="pronunciation-wrap">
-                        <h3 className="header">발음은 어떻게 해야할까요?</h3>
-                        <div>
-                          <h3>{item.pronounce[0]}</h3>
-                        </div>
-                        <div>
-                          <h3>{item.pronounce[1]}</h3>{" "}
-                        </div>
-                        <div>
-                          <h3>{item.pronounce[2]}</h3>
-                        </div>
+            {sampleword.map((item) => {
+              return (
+                <>
+                  {" "}
+                  <h1
+                    className={
+                      "letter timid" +
+                      (highlightedIndex === 0 ? " highlighted" : "")
+                    }
+                  >
+                    {item.lettername}
+                  </h1>
+                  <div
+                    className={
+                      "pronunciation-con timid" +
+                      (highlightedIndex === 1 ? " highlighted" : "")
+                    }
+                  >
+                    <div className="pronunciation-wrap">
+                      <h3 className="header">발음은 어떻게 해야할까요?</h3>
+                      <div>
+                        <h3>{item.pronounce[0]}</h3>
+                      </div>
+                      <div>
+                        <h3>{item.pronounce[1]}</h3>{" "}
+                      </div>
+                      <div>
+                        <h3>{item.pronounce[2]}</h3>
                       </div>
                     </div>
-                    <div
-                      className={
-                        highlightedIndex === 2
-                          ? "practice-con highlighted"
-                          : "practice-con timid"
-                      }
-                    >
-                      <h3 className="header">단어장.</h3>
-                      <div
-                        className={
-                          highlightedIndex === 2
-                            ? "practice-wrap highlighted"
-                            : "practice-wrap "
-                        }
-                      >
-                        <h3>ㅁㄴㅇㄹ</h3>
-                      </div>
+                  </div>
+                  <div
+                    className={
+                      highlightedIndex === 2
+                        ? "practice-con highlighted"
+                        : "practice-con timid"
+                    }
+                  >
+                    <h3 className="header">단어장.</h3>
 
-                      {list.map((ii, index) => {
-                        return (
-                          <>
-                            <div className="practice-wrap add">
-                              <h3 key={index}>{ii}</h3>
-                            </div>
-                          </>
-                        );
-                      })}
-                    </div>{" "}
-                    <div
-                      className={
-                        highlightedIndex === 3
-                          ? "add-container highlighted"
-                          : "add-container timid"
-                      }
-                    >
-                      <div className="add-wrap">
-                        {" "}
-                        <h3>단어를 입력해보세요</h3>
-                        <div className="wrap">
-                          <input
-                            type="text"
-                            //value={obj}
-                            onChange={inputt}
-                          ></input>
-                          <button type="submit" onClick={click}>
-                            입력
-                          </button>
-                        </div>
+                    {list.map((ii, index) => {
+                      return (
+                        <>
+                          <div className="practice-wrap add">
+                            <h3 key={index}>{ii}</h3>
+                          </div>
+                        </>
+                      );
+                    })}
+                  </div>{" "}
+                  <div
+                    className={
+                      highlightedIndex === 3
+                        ? "add-container highlighted"
+                        : "add-container timid"
+                    }
+                  >
+                    <div className="add-wrap">
+                      {" "}
+                      <h3>단어를 입력해보세요</h3>
+                      <div className="wrap">
+                        <input
+                          type="text"
+                          //value={obj}
+                          onChange={inputt}
+                        ></input>
+                        <button type="submit" onClick={click}>
+                          입력
+                        </button>
                       </div>
                     </div>
-                  </>
-                );
-              })}
+                  </div>
+                </>
+              );
+            })}
           </div>
         )}
       </div>
